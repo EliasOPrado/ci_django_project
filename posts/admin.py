349cd5930acb0d403b749ec65d5cd77accda840f
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-02-10 23:12:15 +0000

    added site functionallity.

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+#to show up in the /admin page
+admin.site.register(Post)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Post(models.Model):
+    """
+    A single blog post
+    """
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    views = models.IntegerField(default=0)
+    tags = models.CharField(max_length=30, null=True, blank=True)
+    image = models.ImageField(upload_to="img", blank=True, null=True)
+
+    def __unicode__(self):
+        return self.title
