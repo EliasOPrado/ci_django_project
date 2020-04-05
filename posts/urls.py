ab85c59d96a2f65a0a4d0c020424ad764928c87b
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-04-05 23:55:59 +0100

    changed made in urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from .views import get_posts, post_detail, create_or_edit_post
 
+"""
+All these urls will be included into the main url.py file as post/
+-- These urls are based in all the funtions in posts.views. --
+"""
+
 urlpatterns = [
     url(r'^$', get_posts, name='get_posts'),
     url(r'^(?P<pk>\d+)/$', post_detail, name='post_detail'),
